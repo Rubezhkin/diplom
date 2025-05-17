7687919d0673f918e74059455b94ef42ac79a26f
rubezhkin <pavelned03@gmail.com>
2025-05-17 19:50:59 +0300

    add post controller

---
+++ spring-jpa/supcom/src/main/java/pv/nedobezhkin/supcom/controller/PostController.java
@@ -0,0 +1,92 @@
+package pv.nedobezhkin.supcom.controller;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.apache.coyote.BadRequestException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import pv.nedobezhkin.supcom.service.PostService;
+import pv.nedobezhkin.supcom.service.dto.PostDTO;
+
+@RestController
+@RequestMapping("/api/posts")
+@RequiredArgsConstructor
+public class PostController {
+	private static final Logger LOG = LoggerFactory.getLogger(PostController.class);
+	private final PostService postService;
+
+	@PostMapping("")
+	public ResponseEntity<PostDTO> createPost(@Valid @RequestBody PostDTO postDTO) {
+		LOG.debug("REST request to save Post: {}", postDTO);
+		PostDTO result = postService.save(postDTO);
+		return ResponseEntity.ok().body(result);
+	}
+
+	@GetMapping("")
+	public ResponseEntity<List<PostDTO>> getAllPosts() {
+		LOG.debug("REST request to get all Posts");
+		List<PostDTO> result = postService.findAll();
+		return ResponseEntity.ok().body(result);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<PostDTO> getPostById(@PathVariable Long id) {
+		LOG.debug("REST request to get Post: {}", id);
+		PostDTO result = postService.findById(id).orElse(null);
+		return ResponseEntity.ok().body(result);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<PostDTO> updatePost(@PathVariable Long id, @Valid @RequestBody PostDTO postDTO)
+			throws BadRequestException {
+		LOG.debug("REST request to put Post: {} {}", id, postDTO);
+		if (postDTO.getId() == null) {
+			throw new BadRequestException("id null");
+		}
+		if (!Objects.equals(postDTO.getId(), id)) {
+			throw new BadRequestException("id invalid");
+		}
+		if (postService.findById(id) == null) {
+			throw new BadRequestException("id not found");
+		}
+
+		PostDTO result = postService.update(postDTO);
+		return ResponseEntity.ok().body(result);
+	}
+
+	@PatchMapping("/{id}")
+	public ResponseEntity<PostDTO> partialUpdatePost(@PathVariable Long id,
+			@NotNull @RequestBody PostDTO postDTO) throws BadRequestException {
+		LOG.debug("REST request to patch Post: {} {}", id, postDTO);
+		if (postService.findById(id) == null) {
+			throw new BadRequestException("id not found");
+		}
+		postDTO.setId(id);
+		PostDTO result = postService.partialUpdate(postDTO);
+
+		return ResponseEntity.ok().body(result);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deletePost(@PathVariable Long id) {
+		LOG.debug("REST request to delete post: {}", id);
+		postService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+}
--- spring-jpa/supcom/src/main/java/pv/nedobezhkin/supcom/service/Impl/PostServiceImpl.java
+++ spring-jpa/supcom/src/main/java/pv/nedobezhkin/supcom/service/Impl/PostServiceImpl.java
@@ -1,50 +1,104 @@
 package pv.nedobezhkin.supcom.service.Impl;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import pv.nedobezhkin.supcom.entity.Author;
+import pv.nedobezhkin.supcom.entity.Post;
+import pv.nedobezhkin.supcom.entity.SubscriptionTier;
+import pv.nedobezhkin.supcom.repository.AuthorRepository;
+import pv.nedobezhkin.supcom.repository.PostRepository;
+import pv.nedobezhkin.supcom.repository.SubscriptionTierRepository;
 import pv.nedobezhkin.supcom.service.PostService;
 import pv.nedobezhkin.supcom.service.dto.PostDTO;
+import pv.nedobezhkin.supcom.service.mapper.PostMapper;
 
+@RequiredArgsConstructor
 @Service
 public class PostServiceImpl implements PostService {
 
+	private static final Logger LOG = LoggerFactory.getLogger(PostServiceImpl.class);
+	private final PostRepository postRepository;
+	private final PostMapper postMapper;
+	private final AuthorRepository authorRepository;
+	private final SubscriptionTierRepository subscriptionTierRepository;
+
 	@Override
 	public PostDTO save(PostDTO postDTO) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'save'");
+		LOG.debug("Request to save Post: {}", postDTO);
+		Post post = postMapper.toEntity(postDTO);
+		Author author = authorRepository.findById(postDTO.getAuthor())
+				.orElseThrow(() -> new EntityNotFoundException("Author not found"));
+		SubscriptionTier tier;
+		if (postDTO.getTier() == null)
+			tier = null;
+		else
+			tier = subscriptionTierRepository.findById(postDTO.getTier())
+					.orElseThrow(() -> new EntityNotFoundException("Tier not found"));
+		post.setAuthor(author);
+		post.setTier(tier);
+		post.setCreationTime(ZonedDateTime.now());
+		post = postRepository.save(post);
+		return postMapper.toDto(post);
 	}
 
 	@Override
 	public PostDTO update(PostDTO postDTO) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'update'");
+		LOG.debug("Request to update Post: {}", postDTO);
+		Post post = postMapper.toEntity(postDTO);
+		Author author = authorRepository.findById(postDTO.getAuthor())
+				.orElseThrow(() -> new EntityNotFoundException("Author not found"));
+		SubscriptionTier tier;
+		if (postDTO.getTier() == null)
+			tier = null;
+		else
+			tier = subscriptionTierRepository.findById(postDTO.getTier())
+					.orElseThrow(() -> new EntityNotFoundException("Tier not found"));
+		post.setAuthor(author);
+		post.setTier(tier);
+		post = postRepository.save(post);
+		return postMapper.toDto(post);
 	}
 
 	@Override
 	public PostDTO partialUpdate(PostDTO postDTO) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'partialUpdate'");
+		LOG.debug("Request to partically update Post: {}", postDTO);
+
+		return postRepository
+				.findById(postDTO.getId())
+				.map(existingPost -> {
+					postMapper.partialUpdate(existingPost, postDTO);
+					return existingPost;
+				})
+				.map(postRepository::save)
+				.map(postMapper::toDto).orElse(null);
 	}
 
 	@Override
 	public Optional<PostDTO> findById(Long id) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'findById'");
+		LOG.debug("Request to get Post: {}", id);
+		return postRepository.findById(id).map(postMapper::toDto);
 	}
 
 	@Override
 	public List<PostDTO> findAll() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+		LOG.debug("Request to get all Posts");
+		return postRepository.findAll()
+				.stream().map(postMapper::toDto)
+				.collect(Collectors.toList());
 	}
 
 	@Override
 	public void delete(Long id) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'delete'");
+		LOG.debug("Request to delete Post: {}", id);
+		postRepository.deleteById(id);
 	}
-
 }
